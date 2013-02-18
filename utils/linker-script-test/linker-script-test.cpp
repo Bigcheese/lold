bc85964693de78ffe7b3ba524f4fcf6b656b6a01
Michael J. Spencer <bigcheesegs@gmail.com>
2013-02-18 10:36:11 -0800

    Some code.

---
+++ utils/linker-script-test/linker-script-test.cpp
@@ -0,0 +1,25 @@
+//===- utils/linker-script-test/linker-script-test.cpp --------------------===//
+//
+//                             The LLVM Linker
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// \brief Tool for testing linker script parsing.
+///
+//===----------------------------------------------------------------------===//
+
+#include "llvm/Support/SourceMgr.h"
+
+class Token {
+public:
+  enum Kind {
+    
+  };
+};
+
+int main(int argc, const char **argv) {
+}
